1ce583dc317b9c0bc88bc7db222b80456632d56e
iSamity <gamandsoc@gmail.com>
2025-09-08 19:40:07 +0200

    Add patch for floor tiles

---
+++ Patches/FloorPatch.cs
@@ -0,0 +1,113 @@
+﻿using AirportCEOHistoryCEO.History;
+using AirportCEOHistoryCEO.Models;
+using HarmonyLib;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AirportCEOHistoryCEO.Patches;
+
+[HarmonyPatch]
+internal class AddTileToGridPatch
+{
+    static List<TileableWithOldQuality> tileables = new List<TileableWithOldQuality>();
+
+    internal class AddTileState
+    {
+        public byte OldQuality;
+    }
+
+    [HarmonyPatch(typeof(PlaceableFloor), nameof(PlaceableFloor.AddTileToGrid))]
+    [HarmonyPrefix]
+    internal static void AddTileToGridPrefix(Vector4 position, ref AddTileState __state)
+    {
+        if (!SaveLoadGameDataController.loadComplete)
+        {
+            return;
+        }
+
+
+        if (Singleton<TiledObjectsManager>.Instance.TryGetTileable(position, out var tileable))
+        {
+            __state = new AddTileState
+            {
+                OldQuality = tileable.Quality
+            };
+        }
+    }
+
+    [HarmonyPatch(typeof(PlaceableFloor), nameof(PlaceableFloor.AddTileToGrid))]
+    [HarmonyPostfix]
+    internal static void AddTileToGridPostfix(Vector4 position, ref AddTileState __state)
+    {
+        if (!SaveLoadGameDataController.loadComplete)
+        {
+            return;
+        }
+
+        if (Singleton<TiledObjectsManager>.Instance.TryGetTileable(position, out var tileable))
+        {
+            tileables.Add(new TileableWithOldQuality(tileable, __state.OldQuality));
+        }
+    }
+
+    [HarmonyPatch(typeof(TileMerger), nameof(TileMerger.MergeSurroundingTiles))]
+    [HarmonyPostfix]
+    internal static void TileMergerPostfix(Vector3[] positions, ITileSource tileSource)
+    {
+        if (!SaveLoadGameDataController.loadComplete)
+        {
+            return;
+        }
+
+        if (tileables.Count == 0)
+        {
+            return;
+        }
+
+
+        var action = new FloorAction(tileables);
+
+        tileables.Clear();
+
+        HistoryManager.AddToHistory(action);
+        Plugin.Logger.LogInfo($"Added TileableAction to history for tileable object");
+    }
+
+}
+
+internal class FloorAction : IHistoryAction
+{
+    List<TileableWithOldQuality> tileables { get; set; }
+    public FloorAction(List<TileableWithOldQuality> _tileables)
+    {
+        tileables = new List<TileableWithOldQuality>();
+        tileables.AddRange(_tileables);
+    }
+
+    public void Undo()
+    {
+        tileables.ForEach(t =>
+        {
+            t.tileable.Quality = t.oldQuality;
+        });
+
+        TileMerger.MergeTileablesByType(Enums.TileType.Floor);
+    }
+
+    public void Redo()
+    {
+        //Singleton<TiledObjectsManager>.Instance.AddTileable(tileable);
+    }
+}
+
+
+internal class TileableWithOldQuality
+{
+    public ITileable tileable { get; set; }
+    public byte oldQuality { get; set; }
+    public TileableWithOldQuality(ITileable _tileable, byte _oldQuality)
+    {
+        tileable = _tileable;
+        oldQuality = _oldQuality;
+    }
+}
